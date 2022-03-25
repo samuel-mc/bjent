b248e9622e75ed55e381703799241417b5d20d24
Samuel Medina <smdoomed@gmail.com>
2022-03-25 08:09:28 -0600

    cambios Marzo 25

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+let isShowing = false;
+
 const header = document.querySelector(".header");
+const navbar = document.querySelector(".header__nav");
+const iconHamburguer = document.getElementById('iconHamburguer');
+const iconCross = document.getElementById('iconCross');
+
+
 window.addEventListener("scroll", function(e) {
     if (window.scrollY >  80) {
         header.classList = "header bg";
@@ -6,3 +13,19 @@ window.addEventListener("scroll", function(e) {
         header.classList = "header";
     }
 });
+
+const handleShowNav = () => {
+    if (!isShowing) {
+        isShowing = true;
+        navbar.style.display = "block";
+        iconHamburguer.style.display = "none";
+        iconCross.style.display = "block";
+
+
+    } else {
+        isShowing = false;
+        navbar.style.display = "none";
+        iconHamburguer.style.display = "block";
+        iconCross.style.display = "none";
+    }
+}
